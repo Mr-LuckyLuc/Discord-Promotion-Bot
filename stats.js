f280e7ed7edebe3d45fad3c88a2c546764fde1bf
Lucas <lucas.belmans@student.kdg.be>
2026-02-07 17:15:01 +0100

    fixed filter hopefully

--- stats.js
+++ stats.js
@@ -27,7 +27,7 @@ module.exports = {
         const reformatted = Object.entries(enlisted).map(arr => {arr[1].id = arr[0]; return arr[1]})
         
         const companyLeader = reformatted.filter(enlistee => companyLeaderFilter.includes(enlistee.rank))
-        const companyStaff = reformatted.filter(enlistee => companyLeaderFilter.includes(enlistee.rank) || enlistee.id === me) //juck hardcoded
+        const companyStaff = reformatted.filter(enlistee => companyLeaderFilter.includes(enlistee.rank) && !enlistee.id === me) //juck hardcoded
 
         for (const filter of companyLeaderFilter) {
             for (const person of companyLeader.filter(enlistee => enlistee.rank === filter)) {
@@ -79,7 +79,7 @@ module.exports = {
         for (const unit of Object.keys(units).filter(unit => seperatedUnits.includes(unit))) {
             message += `## ${unit}\n`
 
-            const platoon = reformatted.filter(enlistee => enlistee.unit === unit || enlistee.id === me);
+            const platoon = reformatted.filter(enlistee => enlistee.unit === unit && !enlistee.id === me);
 
             for (const career of Object.keys(careers).filter(career => seperatedCareers.includes(career))) {
                 people = platoon.filter(enlistee => enlistee.career===career)
